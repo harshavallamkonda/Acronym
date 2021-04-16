6c69344b0b15c0598ed9fd458930b48eac74f218
V Venkata Sree Harsha <63708477+harshavallamkonda@users.noreply.github.com>
2021-04-16 20:04:11 +0530

    Update Acronyms.py

--- Acronyms.py
+++ Acronyms.py
@@ -1,7 +1,7 @@
 print("Welcome to Acronym project")
 print("Rules: Just execute the code in terminal and enter names by giving space")
 print("CREATED BY HARSHA VALLAMKONDA")
-user_input = str(input("Enter a Phrase: "))
+user_input = str(input("Enter a phrase to be displayed: "))
 text = user_input.split()
 a = " "
 for i in text:
